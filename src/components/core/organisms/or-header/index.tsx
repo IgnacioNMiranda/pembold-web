3e6cdeb32784e93c2144984138afbcfad69f4e26
IgnacioNMiranda <i.miranda.f99@gmail.com>
2021-09-20 11:56:30 -0300

    fix: icons background and animation

--- src/components/core/organisms/or-header/index.tsx
+++ src/components/core/organisms/or-header/index.tsx
@@ -1,11 +1,10 @@
 import styles from './styles.module.scss';
 import { MlNavbar, MlAuthOptions } from '../../molecules';
-import { CloseMenuIcon, MenuIcon } from '../../../utils/icons';
+import { MenuIcon } from '../../../utils/icons';
 import { useEffect, useState } from 'react';
 
 export const OrHeader = () => {
   const [headerStyles, setHeaderStyles] = useState(styles.headerContainer);
-  const [iconsBgStyle, setIconsBgStyle] = useState('');
 
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const toggleMobileMenu = () => {
@@ -14,7 +13,6 @@ export const OrHeader = () => {
 
   useEffect(() => {
     setHeaderStyles(isMenuOpen ? `${styles.headerContainer} ${styles.displayedMenu}` : styles.headerContainer);
-    setIconsBgStyle(isMenuOpen ? styles.iconsBg : '');
   }, [isMenuOpen]);
 
   return (
@@ -22,9 +20,7 @@ export const OrHeader = () => {
       <div className={headerStyles}>
         <div className={styles.menu}>
           <div className={styles.menuIcons}>
-            <div className={iconsBgStyle} />
             <MenuIcon onClick={toggleMobileMenu} isOpen={isMenuOpen} />
-            <CloseMenuIcon onClick={toggleMobileMenu} isOpen={isMenuOpen} />
           </div>
           <div className={styles.menuItems}>
             <MlNavbar />
